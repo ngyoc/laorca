16bb757cf08e7700789ecad71c13d32994bc0eef
ngyoc <yoshi.kou.125@gmail.com>
2022-07-29 13:02:20 +0900

    add: functions.php追加 カスタムメニュー実装

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+// カスタムメニューを追加
+function menu_setup()
+{
+    register_nav_menus(array(
+        'global' => 'グローバルメニュー',
+        'side'   => 'サイドメニュー',
+        'footer' => 'フッターメニュー',
+    ));
+}
+
+add_action('after_setup_theme', 'menu_setup');
