dc75eaa055efae0c41363eafaa2b5191bb324516
ngyoc <yoshi.kou.125@gmail.com>
2022-08-08 10:28:01 +0900

    ヒーローセクションをパーツ化

--- index.php
+++ index.php
@@ -4,22 +4,8 @@
 <section class="section--hero">
 <div class="section--inner">
 
+<?php get_template_part('template/section_hero', null, $args); ?>
 
-    <div class="hero-wrap">
-        <div class="hero-image">        </div><!-- /.hero-image -->
-        <div class="hero-box">
-            <div class="hero-subtext">Web Production</div><!-- /.hero-subtext -->
-            <div class="hero-title">LA ORCA</div><!-- /.hero-title -->
-            <div class="hero-description">
-                <p>BtoBには特有のマーケティング、採用、プロダクトの課題があります。<br>BtoBの勝ちパターンを網羅した180項目におよぶ独自のメソッドと140項目に及ぶ独自の制作ワークフローを駆使しながらマーケティング、デザイン、テクノロジーを組み合わせてBtoB企業の経営課題を丁寧に解決します。</p>
-            </div><!-- /.hero-description -->
-
-            <div class="hero-contact-wrap flex">
-                    <a href="#" class="hero-btn btn-box-black">制作について問い合わせる</a>
-            </div><!-- /.hero-contact-wrap -->
-
-        </div><!-- /.hero-box -->
-    </div><!-- /.hero-wrap -->
 
 </div><!-- /.section--inner -->
 </section><!-- /.hero -->
---
+++ template/section_hero.php
@@ -0,0 +1,19 @@
+<div class="hero-wrap">
+    <div class="hero-image"></div><!-- /.hero-image -->
+
+    <div class="hero-box">
+
+        <div class="hero-subtext">
+            Web Production
+        </div><!-- /.hero-subtext -->
+        <div class="hero-title">LA ORCA</div><!-- /.hero-title -->
+        <div class="hero-description">
+            <p>BtoBには特有のマーケティング、採用、プロダクトの課題があります。<br>BtoBの勝ちパターンを網羅した180項目におよぶ独自のメソッドと140項目に及ぶ独自の制作ワークフローを駆使しながらマーケティング、デザイン、テクノロジーを組み合わせてBtoB企業の経営課題を丁寧に解決します。</p>
+        </div><!-- /.hero-description -->
+
+        <div class="hero-contact-wrap flex">
+                <a href="#" class="hero-btn btn-box-black">制作について問い合わせる</a>
+        </div><!-- /.hero-contact-wrap -->
+
+    </div><!-- /.hero-box -->
+</div><!-- /.hero-wrap -->
