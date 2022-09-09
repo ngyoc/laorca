765b9cae8faa6b3c215ed6061d6acb26e9688fc5
ngyoc <yoshi.kou.125@gmail.com>
2022-09-09 10:49:25 +0900

    taxonomy.phpを追加

---
+++ taxonomy.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<section class="section--archives">
+    <div class="section--inner">
+
+        <p>This is taxonomy.php</p>
+
+        <!-- <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+            <?php endwhile;
+                else : ?>
+            <p>記事はありません</p>
+        <?php endif; ?> -->
+
+
+
+    </div>
+</section>
+
+<?php get_footer(); ?>
