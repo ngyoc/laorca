7f4e7df4f3b78134a12da77e05d876bf3a4c0341
ngyoc <yoshi.kou.125@gmail.com>
2022-09-09 10:48:05 +0900

    カスタム投稿一覧に記事を表示させる

--- functions.php
+++ functions.php
@@ -60,6 +60,7 @@ function create_post_type()
         'has_archive'   => false, // アーカイブ機能ON/OFF
         'menu_position' => 5,     // 管理画面上での配置場所
         'show_in_rest'  => true,  // 5系から出てきた新エディタ「Gutenberg」を有効にする
+        'exclude_From_Search' => false,
         'supports' => array('title', 'editor', 'thumbnail')
     ]);
 
