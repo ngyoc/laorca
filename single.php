3cb5527c17d995aee14c939138eb52c8ee37dc9a
ngyoc <yoshi.kou.125@gmail.com>
2022-09-30 17:05:05 +0900

    細かいHTML修正

--- single.php
+++ single.php
@@ -1,13 +1,62 @@
 <?php get_header(); ?>
 
-<section class="section--news section-top section-post">
+<div class="single-header">
+
+    <div class="single-type">
+        <?php $title_args = [
+            'title_slag'    => 'blog',
+            'page_title'    => 'BLOG',
+            'page_subtitle' => 'ブログ',
+        ];
+
+        get_template_part('template/echo_section_title', null, $title_args);
+        ?>
+
+    </div>
+
+</div>
+
+
+<section class="section--single ">
     <div class="section--inner">
 
-        <p>This is single.php</p>
+        <div class="post-about">
+
+            <div class="post-title">
+                <h1><?php the_title(); ?></h1>
+            </div><!-- /.single-title -->
+
+            <div class="post-category">
+                <?php $taxonomy_args = [
+                    'taxonomy_slag' => 'works-cat',
+                    'flag_link' => false,
+                    'cat_or_tag' => 'cat'
+                ]
+                ?>
+
+                <p>カテゴリー：<?php get_template_part('template/echo_post_taxonomy', null, $taxonomy_args); ?></p>
+
+            </div><!-- /.single-category -->
+
+            <div class="post-tag">
+                <?php $taxonomy_args = [
+                    'taxonomy_slag' => 'works-tag',
+                    'flag_link' => false,
+                    'cat_or_tag' => 'tag'
+                ]
+                ?>
+
+                <p>タグ：<?php get_template_part('template/echo_post_taxonomy', null, $taxonomy_args); ?></p>
+
+            </div><!-- /.post-tag -->
+
+        </div><!-- /.single-about -->
 
         <div class="post-content">
             <?php the_content(); ?>
         </div><!-- /.post-content -->
+
+        <?php get_template_part('template/link_post_nextprev') ?>
 
     </div>
 </section>
